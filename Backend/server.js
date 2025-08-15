a761a1144884a5ca481c4c9c25712ee90d38411f
lamiodi <tygaodibenuah@gmail.com>
2025-08-15 09:19:07 +0100

    Prepare backend for Render deployment

--- Backend/server.js
+++ Backend/server.js
@@ -71,7 +71,7 @@ app.use((err, req, res, next) => {
 });
 
 // Health check
-app.get('/', (req, res) => res.send('E-commerce API running!'));
+app.get('/', (req, res) => res.send('TIA Backend is running 🚀'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
